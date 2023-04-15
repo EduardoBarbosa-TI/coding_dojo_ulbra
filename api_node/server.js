3dd739f8564584b9cef5401c83e6bc20dd382ef5
EduardoBarbosa-TI <eduardobb@rede.ulbra.br>
2023-04-15 13:40:54 -0300

    Instalando express para lidar com as rotas da api e criando arquivo json como base de dados

---
+++ api_node/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const baloes = require("./database/baloes.json");
+
+app.get("/baloes", (req,res) => {
+
+
+    res.send(baloes)
+})
+
+app.get("/baloes/:id", (req,res) => {
+    const id = req.params.id
+
+    const baloesEncontrados = baloes.filter((balao) => balao.id == id) 
+
+    if(baloesEncontrados.length == 0){
+        res.status(404).send()
+    }
+
+    const balao = baloesEncontrados[0]
+    res.send(balao)
+    
+}) 
+
+app.listen(port);
---
+++ dojo.js
@@ -0,0 +1,11 @@
+//1-Criarmos o repositório no git
+//FEITO
+//2-Clonar o projeto no local
+//FEITO
+//3-criarmos a estrutura da api
+//FEITO
+//4-Criarmos a consulta de dados em um JSON
+
+//5-subir a api para o git 
+
+//6-Fazer o deploy
